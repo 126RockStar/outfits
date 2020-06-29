43c5d55a7c099a6dd483d1e4548025c69db0135a
Admin <outfitszone@yahoo.com>
2020-06-29 06:26:32 +0000

    nav buttons style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,11 +95,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link btn btn-outline-info btn-sm mr-1" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link btn btn-outline-info btn-sm mr-1" style="padding: .1rem" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link btn btn-outline-info btn-sm" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link btn btn-outline-info btn-sm" style="padding: .1rem" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
